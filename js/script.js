f70fda1013081aaee850581d065c997826bf1db2
Natnael Sisay <natyjava8@gmail.com>
2020-09-14 18:03:24 +0300

    finished up the image toogle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('working state');
+const menuBtn = document.getElementById('menu-btn');
+var menuBtnImg = document.getElementById('menu-btn-img');
+const img_1 = './images/icon-hamburger.svg';
+const img_2 = './images/icon-close.svg';
+let state = false;
+
+menuBtnImg.onclick = function () {
+  if (!state) {
+    menuBtnImg.src = img_2;
+    state = true;
+  } else {
+    menuBtnImg.src = img_1;
+    state = false;
+  }
+};

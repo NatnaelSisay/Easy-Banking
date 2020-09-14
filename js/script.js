eb866db70b9c14dd4882641c2a630e07afff60a2
Natnael Sisay <natyjava8@gmail.com>
2020-09-14 19:27:15 +0300

    finished up drop down navigation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.getElementById('menu-btn');
 var menuBtnImg = document.getElementById('menu-btn-img');
+var dropDown = document.getElementById('main-nav');
 const img_1 = './images/icon-hamburger.svg';
 const img_2 = './images/icon-close.svg';
 let state = false;
@@ -7,9 +8,11 @@ let state = false;
 menuBtnImg.onclick = function () {
   if (!state) {
     menuBtnImg.src = img_2;
+    dropDown.style['display'] = 'block';
     state = true;
   } else {
     menuBtnImg.src = img_1;
+    dropDown.style['display'] = 'none';
     state = false;
   }
 };
